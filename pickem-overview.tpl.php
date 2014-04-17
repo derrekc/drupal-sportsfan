62c7cfc2ab115a5d6912c80e0d28b16e6059af93
Derrek Croney <derrek@decro.net>
2014-04-17 17:00:18 -0400

    great progress in creating new workflow for pickem manager

---
+++ pickem-overview.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h2>Welcome Back...</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8">
+			<h2>Upcoming Games To Pick</h2>
+		</div>
+		<div class="col-md-4">
+			<h2>Standings</h2>
+		</div>
+	</div>
+</div>

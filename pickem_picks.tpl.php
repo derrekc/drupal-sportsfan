1d258b2ae50a0df4b4200df8499185d681ee1eeb
derrekc <dlc32@duke.edu>
2014-04-16 21:47:00 -0400

    added new template

--- pickem_picks.tpl.php
+++ pickem_picks.tpl.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
 	<?php foreach($entries as $row): ?>
 	<div class="row">
-	<div class="col-md-1"><?php print $row['gsx$week']; ?></div>
+	<div class="col-md-1"><?php print $row['gsx$week']['$t']; ?></div>
 	</div>
 	<?php endforeach; ?>
 </div>

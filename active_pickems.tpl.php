c34fe82dd2d356898ea96f240018d3a85e1c33e9
Derrek Croney <derrek@decro.net>
2014-04-24 13:07:57 -0400

    general changes/edits

--- active_pickems.tpl.php
+++ active_pickems.tpl.php
@@ -4,5 +4,11 @@
 		<?php if (!empty($pickem->description)): ?>
 			<p class=" bg-info"><?php print $pickem->description; ?></p>
 		<?php endif; ?>
+		<?php if ($pickem->user_can_play): ?>
+			<a class="btn btn-primary" href="/pickem/<?php print $pickem->name; ?>">Overview</a>&nbsp;
+			<a class="btn btn-default" href="/pickem/<?php print $pickem->name; ?>/slate">Play / Make Picks</a>&nbsp;
+		<?php else: ?>
+			<a class="btn btn-primary" href="/user">Join Today!</a>
+		<?php endif; ?>
 	</div>
 <?php endforeach; ?>

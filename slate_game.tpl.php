5793a8f0158d3ca419d6d57bace18afce8bc9746
Derrek Croney <derrek@decro.net>
2014-04-24 13:01:09 -0400

    tweaks to game (for pick) display

--- slate_game.tpl.php
+++ slate_game.tpl.php
@@ -3,7 +3,10 @@
 		<img height="30" src="<?php print base_path() . drupal_get_path('module', 'pickem') . '/logos/ncaaf/' . $slate_event['#event']->visiting_school_name; ?>.gif" />
 		<img height="30" src="<?php print base_path() . drupal_get_path('module', 'pickem') . '/logos/ncaaf/' . $slate_event['#event']->host_school_name; ?>.gif" />
 	</div>
-	<div class="col-xs-3 col-md-4"><?php print render($slate_event['event_title']); ?></div>
+	<div class="col-xs-3 col-md-4">
+		<?php print render($slate_event['event_title']); ?><br />
+		<small><?php print $slate_event['#event']->gameDate('long'); ?></small>
+	</div>
 	<?php foreach(element_children($slate_event['pick_school_name']) as $pick): ?>
 		<div class="col-xs-3 col-md-3">
 			<?php print render($slate_event['pick_school_name'][$pick]); ?>

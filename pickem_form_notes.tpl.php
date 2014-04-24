3ebaecd09cc28c2d1830cdd597533b377e95ecd2
Derrek Croney <derrek@decro.net>
2014-04-24 13:04:00 -0400

    added new template for 'developer notes'

---
+++ pickem_form_notes.tpl.php
@@ -0,0 +1,21 @@
+<div class="panel panel-info">
+  <div class="panel-heading">
+    <h3 class="panel-title">Pickem UI Development Notes</h3>
+  </div>
+  <div class="panel-body">
+	<h4>Layout</h4>
+	<p>I started to prefer a view with one game per line, while simplifying the information displayed.  <em>For instance, 
+		no location info, no team records info.</em>
+	</p>
+	<p>I think I can include the game date, as it'll be needed to make 
+		players aware of deadlines.
+	</p>
+	<h4>Workflow for Making Picks</h4>
+	<ul>
+		<li>Clicking a button (e.g. "Pick Duke") will <strong><em>temporarily</em></strong> save the pick.</li>
+		<li>Picks aren't finalized until the <strong><em>Finalize Picks</em></strong> is clicked.
+	</ul>
+	<p>Is that easier than last season's design?</p>
+	</div>
+</div>
+<div class="clearfix"></div>

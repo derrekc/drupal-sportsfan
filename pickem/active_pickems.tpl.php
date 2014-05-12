15c76525ef2ac22b7cdd5f2c4e85534e2f85a7b8
Derrek Croney <derrek@decro.net>
2014-05-12 11:07:22 -0400

    rearranged structure of module, renaming to 'sportsfan'

--- pickem/active_pickems.tpl.php
+++ pickem/active_pickems.tpl.php
@@ -1,10 +1,16 @@
 <?php foreach($pickems as $pickem): ?>
 	<div class="col-md-12">
+
 		<h3><?php print $pickem->title; ?></h3>
 		<?php if (!empty($pickem->description)): ?>
 			<p><?php print $pickem->description; ?></p>
 		<?php endif; ?>
 
+		<?php
+		/**
+		 * Print the action buttons associated with this Pickem.
+		 */
+		?>
 		<?php foreach ($pickem->ui_actions as $b): ?>
 			<?php print render($b); ?>
 		<? endforeach; ?>

ed82d9dc98ce2f67d81ca37852985658ee8e9671
Derrek Croney <derrek@decro.net>
2014-07-07 13:11:16 -0400

    massive re-factoring of last year's pickem engine

---
+++ pickem/slate_sportsevent.tpl.php
@@ -0,0 +1,11 @@
+<div class="row game-pick-row">
+	<div class="hidden-xs col-md-2">
+		<img height="32" src="<?php print base_path() . drupal_get_path('module', 'team') . '/logos/ncaaf/' . $slate_event['#event']->getSportsEvent()->visiting_team_name; ?>.gif" />
+		<img height="32" src="<?php print base_path() . drupal_get_path('module', 'team') . '/logos/ncaaf/' . $slate_event['#event']->getSportsEvent()->host_team_name; ?>.gif" />
+	</div>
+	<div class="hidden-xs col-sm-4"><?php print render($slate_event['event_title']); ?></div>
+	<div class="col-xs-4 visible-xs"><?php print render($slate_event['event_title_mobile']); ?></div>
+	<div class="col-xs-8 col-md-6">
+		<?php print render($slate_event['pick_team_name']); ?>
+	</div>
+</div>

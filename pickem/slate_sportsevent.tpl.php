44d3132afdaf7436dcd7445acf90e30f5afde7ec
Derrek Croney <derrek@decro.net>
2014-08-06 10:55:27 -0400

    wholesale improvements

--- pickem/slate_sportsevent.tpl.php
+++ pickem/slate_sportsevent.tpl.php
@@ -3,9 +3,9 @@
 		<img height="32" src="<?php print base_path() . drupal_get_path('module', 'team') . '/logos/ncaaf/' . $slate_event['#event']->getSportsEvent()->visiting_team_name; ?>.gif" />
 		<img height="32" src="<?php print base_path() . drupal_get_path('module', 'team') . '/logos/ncaaf/' . $slate_event['#event']->getSportsEvent()->host_team_name; ?>.gif" />
 	</div>
-	<div class="hidden-xs col-sm-4"><?php print render($slate_event['event_title']); ?></div>
-	<div class="col-xs-4 visible-xs"><?php print render($slate_event['event_title_mobile']); ?></div>
-	<div class="col-xs-6 col-md-5">
+	<div class="hidden-xs col-sm-5"><?php print render($slate_event['event_title']); ?></div>
+	<div class="col-xs-5 visible-xs"><?php print render($slate_event['event_title_mobile']); ?></div>
+	<div class="col-xs-5 col-md-4">
 		<?php print render($slate_event['pick_team_name']); ?>
 	</div>
 	<div class="col-xs-1 col-md-1">
